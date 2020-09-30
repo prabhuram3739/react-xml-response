33f608b48df7ce3cdd688a8e2c74cd7bba752ac5
Murugesan <PMurugesan@ad.harman.com>
2020-09-30 20:40:17 +0530

    Added the auto refresh and disable therefresh functionality, added the different menu views for the tabs

--- src/DefaultViewDataLayerContext.js
+++ src/DefaultViewDataLayerContext.js
@@ -29,7 +29,7 @@ componentWillUnmount() {
   clearTimeout(this.intervalID);
 }
 
-componentWillReceiveProps(nextProps){
+/*componentWillReceiveProps(nextProps){
   this.setState({refresh: nextProps.refresh, refreshStatus: nextProps.refreshStatus});
   console.log("Initial Values:", nextProps.refresh, nextProps.refreshStatus);
   console.log("Initial Refresh Status:", this.state.refreshStatus);
@@ -39,34 +39,63 @@ componentWillReceiveProps(nextProps){
     this.getDefaultViewData(this.props.imsi, nextProps.refresh, this.state.refreshStatus);
   }
   
+}*/
+
+static getDerivedStateFromProps(nextProps, prevState) {
+  if(nextProps.refreshStatus !== prevState.refreshStatus) {
+    return {refresh: nextProps.refresh, refreshStatus: nextProps.refreshStatus};
+  } else { 
+   return null 
+  }
+}
+
+componentDidUpdate(prevProps, prevState) {
+ if(prevProps.refreshStatus !== this.props.refreshStatus) {
+   console.log("Inside the ComponentdidUpdate:", prevProps.refreshStatus, this.props.refreshStatus);
+   //Perform some operation here
+   this.setState({refresh: prevProps.refresh, refreshStatus: prevState.refreshStatus});
+   if(this.state.refreshStatus === false) {
+    this.getDefaultViewData(this.props.imsi, prevProps.refresh, prevProps.refreshStatus);
+  } else {
+    this.getDefaultViewData(this.props.imsi, this.state.refresh, this.state.refreshStatus);
+  }
+ }
 }
 
 getDefaultViewData = (imsi, refresh, status) => {
   var self = this;
   imsi ="234500010400205";
+  console.log(this.state.refresh);
+  console.log(this.state.refreshStatus);
+  if(refresh === undefined) {
+    refresh = true;
+  }
+  if(status === undefined) {
+    status = true;
+  }
+  if(refresh !== undefined && status !== undefined) {
   axios
     .get(authEndpoint + "/api/diagnosticData/defaultView/" + imsi, {
         "Content-Type": "application/xml; charset=utf-8"
      })
     .then(function(response) {
-        self.setState((state, props) => ({ loading: false, data: response.data, count: Object.keys(response.data).length }));
+        self.setState((state, props) => ({ loading: false, data: response.data, count: Object.keys(response.data).length, refresh: refresh, refreshStatus: status }));
         console.log("Inside the method:", refresh, status);
         if((status === true && refresh === true) || (status === undefined && refresh === undefined)) {
-          if((refresh === true) || (refresh === undefined)) {
             self.intervalID = setTimeout(self.getDefaultViewData.bind(this), 3000);
-          }
         }
     })
     .catch(function(error) {
         console.log(error);
     });
+  }
 }
 
   render() {
-    const {data, count, loading, refresh} = this.state || {};
+    const {data, count, loading, refresh, refreshStatus} = this.state || {};
     const {componentDidMount} = this;
     return(
-      <DefaultViewDataLayerContext.Provider value = {{ data, count, loading, refresh, componentDidMount }} > { this.props.children } 
+      <DefaultViewDataLayerContext.Provider value = {{ data, count, loading, refresh, refreshStatus, componentDidMount }} > { this.props.children } 
       </DefaultViewDataLayerContext.Provider>
     )
   }
