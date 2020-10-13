77d8f876094ff5020540cea7efd071589f2506c8
Murugesan <PMurugesan@ad.harman.com>
2020-10-13 10:40:20 +0530

    Added the search box in the main js and aligned the card decks

--- src/main/Main.js
+++ src/main/Main.js
@@ -33,17 +33,17 @@ export default class Main extends React.Component {
                                 <div className="card-deck">
                         <div className="col-2 mt-1 pl-0">
                         <div className="card mb-3 shadow-soft p-3">
-                        <div className="card-header"><FontAwesomeIcon icon = { faUser } className="mr-2" />Accounts</div>
+                        <div className="card-header"><FontAwesomeIcon icon = { faUser } className="mr-4" />Accounts</div>
                         </div>
                         </div>
                         <div className="col-2 mt-1 pl-0">
                         <div className="card mb-3 shadow-soft p-3">
-                        <div className="card-header"><FontAwesomeIcon icon = { faUserFriends } className="mr-2" /> Sub-Accounts</div>
+                        <div className="card-header"><FontAwesomeIcon icon = { faUserFriends } className="mr-4" /> Sub-Accounts</div>
                         </div>
                         </div>
                         <div className="col-2 mt-1 pl-0">
                         <div className="card mb-3 shadow-soft p-3">
-                        <div className="card-header"><FontAwesomeIcon icon = { faUsers } className="mr-2" /> Connections</div>
+                        <div className="card-header"><FontAwesomeIcon icon = { faUsers } className="mr-4" /> Connections</div>
                         </div>
                         </div>
 
