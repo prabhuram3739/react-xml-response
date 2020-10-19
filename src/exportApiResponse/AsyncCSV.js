90d2e9a9236475b2aaa38162768d7555b28fda34
Murugesan <PMurugesan@ad.harman.com>
2020-10-19 15:43:05 +0530

    Modified data parsing login to extract the data from the api respone

--- src/exportApiResponse/AsyncCSV.js
+++ src/exportApiResponse/AsyncCSV.js
@@ -9,7 +9,6 @@ const headers = [
 ];
 
 
-
 class AsyncCSV extends Component {
 
   constructor(props) {
@@ -27,21 +26,26 @@ class AsyncCSV extends Component {
 
     // API call to get data 
     const objReport = {
-      filename: 'Clue_Mediator_Report_Async.csv',
+      filename: 'Circuit_Switch_API_Response_Data.csv',
       headers: headers,
-      data: this.props.csvData
+      data: this.props.csvData[0].circuitSwitch
     };
     this.setState({ csvReport: objReport }, () => {
       console.log("Successfully exported");
     });
   }
 
+  checkNested(obj, level, ...rest) {
+    if (obj === undefined) return false;
+    if (rest.length === 0 && obj.hasOwnProperty(level)) return true;
+    return this.checkNested(obj[level], ...rest);
+  }
+
+
   render() {
-
-
     return (
       <CSVLink
-      data={this.props.csvData}
+      data={[this.props.csvData[0].circuitSwitch, ...this.props.csvData[0].circuitSwitch.threeGApnDataList, ...this.props.csvData[0].circuitSwitch.fourGApnDataList]}
         asyncOnClick={true}
         ><button
         className="btn btn-primary pull-right mr-4"
